65178570b691529798fd6c94e9483e857843b88e
DevMarvin <mmwanzi76@gmail.com>
2021-01-10 18:33:14 +0300

    bug fixes

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 import socket
 
-# AF_NET == ipv4 (family/domain)
+# AF_INET == ipv4 (family/domain)
 # SOCK_STREAM == tcp (type)
-s = socket.socket(socket.AF_NET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #setting up localhost
 s.bind((socket.gethostname(), 1234))
 s.listen(5)
@@ -10,3 +10,5 @@ s.listen(5)
 while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established!")
+    clientsocket.send(bytes("Hello there!!!","utf-8"))
+    clientsocket.close()
