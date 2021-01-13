c1ca02dbb4d5a240336d838a510cb39b7f6466a5
DevMarvin <mmwanzi76@gmail.com>
2021-01-13 11:57:01 +0300

    chat server

---
+++ chatApp/server.py
@@ -0,0 +1,71 @@
+import socket
+import select
+
+HEADER_LENGHT = 10
+
+IP = "127.0.0.1"
+PORT = 1234
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+server_socket.bind((IP, PORT))
+server_socket.listen()
+
+sockets_list = [server_socket]
+clients = {}
+
+print(f'Listening for connections on {IP}:{PORT}')
+
+def receive_message(client_socket):
+    try:
+        message_header = client_socket.recv(HEADER_LENGHT)
+
+        if not len(message_header):
+            return False
+
+        message_length = int(message_header.decode("utf-8").strip())
+
+        return {'header': message_header, 'data': client_socket.recv(message_length)}    
+
+    except:
+        # Something went wrong like empty message or client exits abruptly
+        return False    
+
+while True:
+    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+
+    for notified_socket in read_sockets:
+        if notified_socket == server_socket:
+            client_socket, client_address = server_socket.accept()
+            user = receive_message(client_socket)
+            if user == False:
+                continue
+            
+            sockets_list.append(client_socket)
+            clients[client_socket] = user
+            print('')
+        
+        else:
+            message = receive_message(notified_socket)
+
+            if message == False:
+                print('Closed connection from: {}'.format(clients[notified_socket]['data'].decode('utf-8')))
+                sockets_list.remove(notified_socket)
+                del clients[notified_socket]
+
+                continue
+
+            # Iterate over connected clients and broadcast message
+            for client_socket in clients:
+
+                # But don't sent it to sender
+                if client_socket != notified_socket:
+
+                    # Send user and message (both with their headers)
+                    # We are reusing here message header sent by sender, and saved username header send by user when he connected
+                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+
+    # Not really necessary to have this, but will handle some socket exceptions just in case
+    for notified_socket in exception_sockets:
+        sockets_list.remove(notified_socket)
+        del clients[notified_socket]
