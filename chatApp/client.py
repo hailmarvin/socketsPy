39a3d23187ad2ff56909ca1f6aa6d3042dd902d3
DevMarvin <mmwanzi76@gmail.com>
2021-01-15 23:21:34 +0300

    updated clientpy

--- chatApp/client.py
+++ chatApp/client.py
@@ -1,7 +1,65 @@
 import socket
+import select
+import errno
+
+HEADER_LENGTH = 10
+
+IP = "127.0.0.1"
+PORT = 1234
+my_username = input("Username: ")
 
 # create the socket
 # AF_INET == ipv4
 # SOCK_STREAM == TCP
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# not yet completed
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP, PORT))
+# recv() not to block
+client_socket.setblocking(False)
+
+username = my_username.encode("utf-8")
+username_header = f"{len(username):<{HEADER_LENGTH}}".encode("utf-8")
+client_socket.send(username_header + username)
+
+while True:
+    message = input(f"{my_username} > ")
+
+    # Message not empty
+    if message:
+        message = message.encode("utf-8")
+        message_header = f"{len(message):<{HEADER_LENGTH}}".encode("utf-8")
+        client_socket.send(message_header + message)
+
+    try:    
+        # Loop over received messages
+        while True:
+            username_header = client_socket.recv(HEADER_LENGTH)
+
+            if not len(username_header):
+                print('Connection closed by server')
+                sys.exit()
+
+            username_length = int(username_header.decode('utf-8').strip())
+            username = client_socket.recv(username_length).decode('utf-8')
+
+            
+            message_header = client_socket.recv(HEADER_LENGTH)
+            message_length = int(message_header.decode('utf-8').strip())
+            message = client_socket.recv(message_length).decode('utf-8')
+
+            print(f'{username} > {message}')
+
+    except IOError as e:
+        # This is normal on non blocking connections - when there are no incoming data error is going to be raised
+        # Some operating systems will indicate that using AGAIN, and some using WOULDBLOCK error code
+        # We are going to check for both - if one of them - that's expected, means no incoming data, continue as normal
+        # If we got different error code - something happened
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print('Reading error: {}'.format(str(e)))
+            sys.exit()
+
+        #we haven't received anything
+        continue
+
+    except Exception as e:
+        print('Reading error: '.format(str(e)))
+        sys.exit()
